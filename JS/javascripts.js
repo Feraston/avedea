c8f948b1257481e147abe98e3cdbef1ad3d6acf9
Roman Prilyuk <feraston@yandex.ru>
2022-04-22 02:56:18 +0300

    v1.52

--- JS/javascripts.js
+++ JS/javascripts.js
@@ -3,3 +3,19 @@ document.querySelector('.menu__toggle').onclick = () => {
   document.querySelector('.header__button').classList.toggle('header__button-close');
 }
 
+$(document).ready(function(){
+  
+  $(window).scroll(function(){
+      if ($(this).scrollTop() > 100) {
+          $('.scrollup').fadeIn();
+      } else {
+          $('.scrollup').fadeOut();
+      }
+      });
+        
+      $('.scrollup').click(function(){
+      $("html, body").animate({ scrollTop: 0 }, 600);
+      return false;
+  });
+
+});
