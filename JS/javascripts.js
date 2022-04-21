c28ad87dc780aff8e24cb1cfbf856486e3280e9c
Roman Prilyuk <feraston@yandex.ru>
2022-04-21 03:51:22 +0300

    v1.50

--- JS/javascripts.js
+++ JS/javascripts.js
@@ -1,5 +1,5 @@
 document.querySelector('.menu__toggle').onclick = () => {
   document.querySelector('.menu').classList.toggle('menu_is-open');
   document.querySelector('.header__button').classList.toggle('header__button-close');
-  }
+}
 
