32cbce106e0921ae9d6b57b28e9fb67cb1ab71aa
Roman Prilyuk <feraston@yandex.ru>
2022-05-03 02:31:08 +0300

    new Avedea

--- JS/javascripts.js
+++ JS/javascripts.js
@@ -1,21 +1,39 @@
-document.querySelector('.menu__toggle').onclick = () => {
-  document.querySelector('.menu').classList.toggle('menu_is-open');
+document.querySelector('.header__toggle').onclick = () => {
+  document.querySelector('.header__menu').classList.toggle('menu_is-open');
   document.querySelector('.header__button').classList.toggle('header__button-close');
 }
 
-$(document).ready(function(){
-  
-  $(window).scroll(function(){
-      if ($(this).scrollTop() > 100) {
-          $('.scrollup').fadeIn();
-      } else {
-          $('.scrollup').fadeOut();
-      }
-      });
-        
-      $('.scrollup').click(function(){
-      $("html, body").animate({ scrollTop: 0 }, 600);
-      return false;
-  });
+// собираем все якоря; устанавливаем время анимации и количество кадров
+const anchors = [].slice.call(document.querySelectorAll('a[href*="#adress"]')),
+      animationTime = 700,
+      framesCount = 20;
 
+anchors.forEach(function(item) {
+  // каждому якорю присваиваем обработчик события
+  item.addEventListener('click', function(e) {
+    // убираем стандартное поведение
+    e.preventDefault();
+    
+    // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
+    let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
+    
+    // запускаем интервал, в котором
+    let scroller = setInterval(function() {
+      // считаем на сколько скроллить за 1 такт
+      let scrollBy = coordY / framesCount;
+      
+      // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
+      // и дно страницы не достигнуто
+      if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+        // то скроллим на к-во пикселей, которое соответствует одному такту
+        window.scrollBy(0, scrollBy);
+      } else {
+        // иначе добираемся до элемента и выходим из интервала
+        window.scrollTo(0, coordY);
+        clearInterval(scroller);
+      }
+    // время интервала равняется частному от времени анимации и к-ва кадров
+    }, animationTime / framesCount);
+  });
 });
+
