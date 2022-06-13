52316428ab80565520bd5dd45901ef00b8ac13e8
Roman Prilyuk <feraston@yandex.ru>
2022-06-13 23:51:41 +0300

    feat: new disain abon1

--- JS/javascripts.js
+++ JS/javascripts.js
@@ -37,3 +37,21 @@ anchors.forEach(function(item) {
   });
 });
 
+const acc = document.getElementsByClassName("abonement__hidwin");
+
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "flex") {
+      panel.classList.toggle("abonement__winopen");
+    } else {
+      panel.classList.toggle("abonement__winopen");
+    }
+  });
+}
